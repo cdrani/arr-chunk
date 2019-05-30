cfaeb4c8e44212cb1b865a93245ac159257ce5f5
cdrani <charlesdrani@gmail.com>
2019-05-30 13:18:48 -0600

    update variable types

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = (arr, size = 2, clear = false) => {
-  let newArr = []
-  for (var i = 0; i < arr.length; i += size) {
+  const newArr = []
+  for (let i = 0; i < arr.length; i += size) {
     newArr.push(arr.slice(i, i + size))
   }
   return clear ? newArr.filter(arr => arr.length === size) : newArr
