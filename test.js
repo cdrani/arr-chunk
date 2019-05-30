a879a67752e74607636a749b72694e889551a93d
cdrani <charlesdrani@gmail.com>
2019-05-30 13:18:00 -0600

    add size test

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 import test from 'ava'
 import chunk from './'
 
-let arr = [67, 233, 87, 13, 89, 113, 91, 111, 24]
+const arr = [67, 233, 87, 13, 89, 113, 91, 111, 24]
 
 test('chunk defaults work as expected', t => {
   t.deepEqual(chunk(arr), [[67, 233], [87, 13], [89, 113], [91, 111], [24]])
+})
+
+test('chunk with size works as expected', t => {
+  t.deepEqual(chunk(arr, 3), [[67, 233, 87], [13, 89, 113], [91, 111, 24] ])
 })
 
 test('chunk with clear of true returns array with all similar sizes', t => {
